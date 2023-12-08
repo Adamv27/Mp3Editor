9ab4215962c4087f3fcce615a0daa12eb46f86ab
Adam Vinch <adamvinch@gmail.com>
2023-12-08 04:42:17 -0500

    Change to react

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,57 @@
-import { useState } from "react"
+import { useState, useRef } from "react"
 import { Song } from "./components/Song.jsx"
+import { ID3Writer } from 'browser-id3-writer'
+import { convertFileToBuffer, fetchFileAsBuffer } from 'id3-parser/lib/util'
+import parse from 'id3-parser'
 
 import './App.css'
 
 
 function App() {
-  const [MP3Data, setMP3Data] = useState({});
+  const [writer, setWriter] = useState();
+  const [displayedSong, setDisplayedSong] = useState({title: '', artist: '', album: ''});
+
+  let inputRef = useRef(null);
+  
+
+  const uploadImage = () => {
+    console.log('test')   
+  }
+
+
+  const uploadFile = () => {
+    inputRef.current.click();
+  }
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+
+    convertFileToBuffer(file).then(parse).then(tag => {
+      setDisplayedSong({
+        title: tag.title,
+        artist: tag.artist,
+        album: tag.album
+      })
+    })
+
+  }
+  
 
   return (
     <div className='page'>
       <div className='songContainer'> 
         <h1>MP3 Editor</h1>  
-        <button>Upload</button>
-        <button>Download</button>
-        
-        <Song />
+        <div className='button-container'>
+          <button className="input-button" onClick={uploadFile}>Upload</button>
+          <input type='file' accept='audio/mpeg' id='file' ref={inputRef} onChange={handleFileChange} hidden></input>
+          <button className="input-button">Download</button>
+        </div> 
+        <Song
+          uploadImage={() => uploadImage()}
+          title={displayedSong.title}
+          artist={displayedSong.artist}
+          album={displayedSong.album}
+        />
 
       </div>
     </div>
--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -1,10 +1,20 @@
 import React, {useState} from "react"
+import placeHolder from "../assets/placeholder.png"
 
-export const Song = () => {
+import './song.css'
+
+export const Song = ({uploadImage, title, artist, album }) => {
   return(
-    <div>
-      <img src="../assets/placeholder.svg">
+    <div className='song-container'>
+      <img src={placeHolder} onClick={uploadImage}/>
+
+      <label>Title</label>
+      <input type='text' defaultValue={title}></input>
+      <label>Artist</label>
+      <input type='text' defaultValue={artist}></input>
+      <label>Album</label>
+      <input type='text' defaultValue={album}></input>
     </div>
-    )
+  )
 }
 
